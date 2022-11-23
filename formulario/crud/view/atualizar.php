347a9759179b3d154f0e75595d13ed7bf4fe761b
LUCAS GOBI ASSALIM <lucas.gassalim@senacsp.edu.br>
2022-11-23 19:52:25 -0300

    Atualização, login, autenticação

--- formulario/crud/view/atualizar.php
+++ formulario/crud/view/atualizar.php
@@ -1,22 +1,23 @@
 <?php
-//Buscamos o codigo que conecta no SGBD
+//Buscamos o código que conecta no SGBD
 require_once '../bancoDeDados/conecta.php';
-
-$id = $_POST['id'];
+//Recebe os dados que está vindo do formulário
 $nome = $_POST['nome'];
 $turno = $_POST['turno'];
 $inicio = $_POST['inicio'];
-
-$stmt = $bd->prepare(" UPDATE alunos
-                            SET nome = :nome,
-                                turno = :turno
-                                inicio = :inicio
-                            WHERE
-                                id = :id");
-$stmt->bindParam(':nome', $nome);
-$stmt->bindParam(':turno', $turno);
-$stmt->bindParam(':inicio', $inicio);
-
-$stmt->execute();
-
-var_dump($_POST);
+//Faz consulta no BD
+$stmt = $bd->prepare("UPDATE alunos 
+                        SET nome = :nome;
+                        turno = : turno,
+                        incio = :inicio
+                        WHERE
+                            id = :id");
+$stmt-> bindParam(':nome', $nome);
+$stmt-> bindParam(':turno', $turno);
+$stmt-> bindParam(':inicio', $inicio);
+$smtm->execute();
+$atualizou = false;
+if($stmt->rowCount() > 0){
+    $atualizou = true;
+}
+include 'listar.php';
